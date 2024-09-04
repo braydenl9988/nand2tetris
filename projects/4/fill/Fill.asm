f77ccfe6ac93dc4c287f9d8cafc47e2ffe5614d9
braydenl9988 <braydenl9988@gmail.com>
2024-09-03 18:55:00 -0700

    proj 4 complete

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,85 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//psuedo code for this project
+// while (keyPressed) {
+//      SCREEN = -1
+//      SCREEN + 1 = -1
+//      .
+//      .
+//      SCREEN + 511 = -1
+//} else {
+//      SCREEN = 0
+//      SCREEN + 1 = 0
+//      .
+//      .
+//      SCREEN + 511 = 0
+//}
+
+(WHILE)
+    @0
+    D = A
+    @i 
+    M = D //setting counter
+
+    @8192
+    D = A 
+    @n 
+    M = D 
+
+    @KBD
+    D = M //stores keyboard value
+    @KEYPRESSED 
+    D; JGT //jumps if keyboard value > 0 (basically if key is pressed)
+    @KEYRELEASED
+    D; JEQ //jumps if keyboard value == 0 (basically if no key is pressed)
+
+    @WHILE
+    0; JMP
+
+
+(KEYPRESSED)
+    @i 
+    D = M 
+    @n //checking if i >= n
+    D = D - M
+    @WHILE
+    D; JEQ //if (i == n), returns back to the original loop
+
+    @i //checking which RAM register we should be writing -1 too
+    D = M 
+    @SCREEN
+    D = D + A 
+    @currentScreen
+    A = D //sets currentScreen address to the one we want to set to -1
+    M = -1
+
+    @i //increase i for loop conditional
+    M = M + 1
+
+    @KEYPRESSED
+    0; JMP
+
+(KEYRELEASED)
+    @i 
+    D = M 
+    @n //checking if i >= n
+    D = D - M
+    @WHILE
+    D; JEQ //if (i == n), returns back to the original loop
+
+    @i //checking which RAM register we should be writing 0 too
+    D = M 
+    @SCREEN
+    D = D + A 
+    @currentScreen
+    A = D //sets currentScreen address to the one we want to set to 0
+    M = 0
+
+    @i //increase i for loop conditional
+    M = M + 1
+
+    @KEYRELEASED
+    0; JMP
+
+
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+//psuedo code for this project
+// i = 0 
+// multipler = R0
+// n = R1
+// product = R2
+// for (i = 0, i < n, i++) {
+//      product += multipler 
+//}
+
+@0
+D = A
+@i 
+M = D //setting counter
+
+@R2
+M = 0 //resetting R2
+
+(LOOP)
+    @i 
+    D = M 
+    @R1 //checking if i >= R1
+    D = D - M
+    @END 
+    D; JEQ //if (i == n), breaks condition of for loop
+
+    @i //i++ at the beginning of loop
+    M = M + 1
+    @R0
+    D = M //setting value in R0 to D Register to add to value in R2
+    @R2
+    M = M + D //adding value in D Register to value in R2
+
+    @LOOP 
+    0; JMP //unconditional jump to continue loop
+
+(END)
+    @END
+    0; JMP //unconditional jump, end program on infinite loop
+
+
